d7b0b2fdef932ca2edd750d1c9f796db8be8c95f
BaBilonianin <BaBilonianin@users.noreply.github.com>
2018-09-04 22:44:54 +0400

    End day commit. Task 4 dont fix

--- src/insert_sort/task_2_1_4/Solution.java
+++ src/insert_sort/task_2_1_4/Solution.java
@@ -9,40 +9,81 @@ an .n C 1/-element array C. State the problem formally and write pseudocode for
 adding the two integers.
 * */
 
+import java.util.ArrayList;
+
 public class Solution {
+
     public static void main(String[] args) {
-        byte[] A = new byte[]{1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0};
-        byte[] B = new byte[]{1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0};
-        byte[] C = new byte[B.length + 1];
-        doSometihing(A, B, C);
+//        byte[] A = new byte[]{1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0};
+//        byte[] B = new byte[]{1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0};
+        byte[] A = new byte[]{1, 0, 0};
+        byte[] B = new byte[]{1, 0, 0};
+        ArrayList<Byte> C = new ArrayList<>();
+
+//        completeTask(A, B);
+        testGetPow();
+        testCountArrayFromInt();
+        testCountIntFromArray();
     }
 
-    private static void doSometihing(byte[] a, byte[] b, byte[] c) {
+    private static void testGetPow() {
+        System.out.print(getPow(1,2));
+        System.out.print(getPow(2,3));
+        System.out.print(getPow(2,4));
+        System.out.print(getPow(2,5));
+        System.out.print(getPow(2,7));
+    }
+
+    private static void testCountIntFromArray() {
+        countArrayFromInt()
+    }
+
+    private static void testCountArrayFromInt() {
+        countArrayFromInt(5,)
+    }
+
+    private static void completeTask(byte[] a, byte[] b) {
         int intA = countIntFromArray(a);
         int intB = countIntFromArray(b);
-        c = countArrayFromInt(intA + intB);
+        printResults(countArrayFromInt(intA + intB,a.length));
     }
 
-    private static byte[] countArrayFromInt(int i) {
-        
-        return new byte[0];
+    private static void printResults(boolean b,ArrayList<Byte> C) {
+        if (b) {
+            for (byte c : C) {
+                System.out.print(c);
+                System.out.print(" ");
+            }
+            System.out.print("\n");
+        } else System.out.println("C length don't = A.length + 1");
+    }
+
+    private static boolean countArrayFromInt(int i, int length,ArrayList<Byte> C) {
+        C.clear();
+        while (i > 1) {
+            C.add(0, (byte) (i % 2));
+            i = i / 2;
+        }
+        C.add(0, (byte) (i));
+        return C.size() == length + 1;
+
     }
 
     private static int countIntFromArray(byte[] a) {
         int result = 0;
-        for (int i = 0; i < a.length; i++) {
-            result = a[i] == 0 ? result : result + getPow(2, i);
+        for (int i = a.length-1; i > -1; i--) {
+            result = a[i] == 0 ? result : result + getPow(2, i+1);
         }
-        return 0;
+        return result;
     }
 
     private static int getPow(int base, int exponent) {
-        int i  = 1, result = base;
+        int i = 1, result = base;
         while (i < exponent) {
             result = result * base;
             i++;
         }
-        return base;
+        return exponent == 0 ? 1 : result;
     }
 
 
