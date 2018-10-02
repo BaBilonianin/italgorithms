8c7173f913dfe595f447dd6b591710e25dc85647
BaBilonianin <babilonianin@rambler.ru>
2018-10-02 18:40:06 +0300

    Done radix sort.

--- src/radixSort/Solution.java
+++ src/radixSort/Solution.java
@@ -1,7 +1,9 @@
 package radixSort;
 
+import java.util.Arrays;
+
 /*
-* */
+ * */
 public class Solution {
     public static void main(String... args) {
         int[] arr1 = new int[]{1, 0, 4, 3, 5, 7, 1, 3, 4, 2, 6, 8, 2};
@@ -14,51 +16,41 @@ public class Solution {
     }
 
     private static void radixSort(int[][] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            int[] guideArray = getArrayForSorting(i, arr);
-            countingSort.Solution.countingSort(guideArray, guideArray.length);
-            sortSourceArray(guideArray, arr, i);
+        int[][] resultArray = arr;
+        for (int i = arr[0].length - 1; i >= 0; i--) {
+            resultArray = countingSortForTwoDimensionalArray(resultArray, 10, i);
         }
-        for (int i : resultArray)
-            System.out.print(i + " ");
+        for (int[] i : resultArray)
+            System.out.print(Arrays.toString(i) + "\n");
     }
 
-//    private static void sortSourceArray(int[] guideArray, int[][] arr, int i) {
-//        int[][] temp = new int[arr.length][];
-//        for (int j = 0; j < guideArray.length; j++)
-//            temp[j] = arr[j];
-//
-//    }
-
-    private static int[] getArrayForSorting(int[][] arr,int i) {
+    private static int[] getArrayForSorting(int[][] arr, int i) {
         int[] result = new int[arr.length];
         for (int g = 0; g < arr.length; g++)
-            result[g] = arr[i][g];
+            result[g] = arr[g][i];
         return result;
     }
 
-    public static int[][] countingSortForTwoDimensionalArray(int[][] arr, int maxElement,int x) {
+    private static int[][] countingSortForTwoDimensionalArray(int[][] arr, int maxElement, int x) {
         int[] countArray = new int[maxElement + 1];
         int[][] resultArray = new int[arr.length][];
 
 
-        int[] auxiliaryArray = getArrayForSorting(arr,x);
+        int[] auxiliaryArray = getArrayForSorting(arr, x);
 
         for (int j = 0; j < auxiliaryArray.length; j++) {
             countArray[auxiliaryArray[j]]++;
         }
 
-        for (int i = 0; i < countArray.length-1; i++) {
-            countArray[i+1]=countArray[i]+countArray[i+1];
+        for (int i = 0; i < countArray.length - 1; i++) {
+            countArray[i + 1] = countArray[i] + countArray[i + 1];
         }
 
-        for (int i = arr.length-1; i >= 0; i--) {
-            resultArray[countArray[auxiliaryArray[i]]-1]=auxiliaryArray[i];
+        for (int i = arr.length - 1; i >= 0; i--) {
+            resultArray[countArray[auxiliaryArray[i]] - 1] = arr[i];
             countArray[auxiliaryArray[i]]--;
         }
-
-        for (int i : resultArray)
-            System.out.print(i + " ");
+        return resultArray;
     }
 }
 
